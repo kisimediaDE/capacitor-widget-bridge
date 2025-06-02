29425bb8b8f3d2ce280eed4600fccd1dc6a1c0f4
Kisimedia.de <kontakt@kisimedia.de>
2025-06-02 13:45:08 +0200

    Small updates

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,7 +4,7 @@ export default {
     {
       file: 'dist/plugin.js',
       format: 'iife',
-      name: 'capacitorWidgetsBridgePlugin',
+      name: 'CapacitorWidgetsbridgePluginExtented',
       globals: {
         '@capacitor/core': 'capacitorExports',
       },
